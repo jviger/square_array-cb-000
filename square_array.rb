ef5a8e905365fd7398ff8de858ef105bc9572f01
Jason Viger <jason.viger@gmail.com>
2018-08-13 10:20:16 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  location = 0
+  array2 = []
+  while location < array.size do
+    array2[location] = array[location] * array[location]
+    location += 1
+  end
+  
+  return array2
+    
 end
